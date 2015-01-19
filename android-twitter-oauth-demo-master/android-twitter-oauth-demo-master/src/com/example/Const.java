4b0ea385f1268e8ee9972db92ba922c80b5296e6
kunwarandroid <abuagrabaa@gmail.com>
2015-01-19 12:04:27 +0530

    Twitter Demo Classes
    How to integrate twitter

---
+++ android-twitter-oauth-demo-master/android-twitter-oauth-demo-master/src/com/example/Const.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Const {
+    public static final String CONSUMER_KEY = "gM7PVeByHIVYPcoxDsb3ppcoZ";
+    public static final String CONSUMER_SECRET = "3yBSMdqAztSFZMTOsUNvYEfS3cD46nDY7VTe747hIudCHHmOPA";
+    public static final String CALLBACK_URL = "myapp://oauth";
+
+    public static final String IEXTRA_AUTH_URL = "auth_url";
+    public static final String IEXTRA_OAUTH_VERIFIER = "oauth_verifier";
+    public static final String IEXTRA_OAUTH_TOKEN = "oauth_token";
+
+    public static final String PREF_NAME = "com.example.android-twitter-oauth-demo";
+    public static final String PREF_KEY_ACCESS_TOKEN = "access_token";
+    public static final String PREF_KEY_ACCESS_TOKEN_SECRET = "access_token_secret";
+}
